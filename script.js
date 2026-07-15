c36b2416e02589f8c0add9d8fb5fca8afe1ecaa6
sandhiya2203 <107338809+sandhiya2203@users.noreply.github.com>
2026-07-15 10:25:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+async function loadPatientData()
+{
+
+    try
+    {
+
+        const response = await fetch(
+            "/patient_data1.json?time=" + new Date().getTime()
+        );
+
+
+        const data = await response.json();
+
+
+        console.log("JSON DATA:", data);
+
+
+
+        document.getElementById("status").innerHTML =
+            data.status;
+
+
+
+        document.getElementById("heart").innerHTML =
+            data.heartRate;
+
+
+
+        document.getElementById("bp").innerHTML =
+            data.bloodPressure;
+
+
+
+        document.getElementById("spo2").innerHTML =
+            data.spo2;
+
+
+
+    }
+
+    catch(error)
+    {
+
+        console.log(
+            "JSON ERROR:",
+            error
+        );
+
+    }
+
+
+}
+
+
+
+function updateDateTime()
+{
+
+    let now = new Date();
+
+
+    document.getElementById("date").innerHTML =
+        now.toLocaleDateString();
+
+
+    document.getElementById("time").innerHTML =
+        now.toLocaleTimeString();
+
+
+}
+
+
+
+
+setInterval(loadPatientData,1000);
+
+
+setInterval(updateDateTime,1000);
+
+
+loadPatientData();
+
+updateDateTime();
