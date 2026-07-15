8e4e533f833a7db163ade2ead0b20676c8ad4f0f
sandhiya2203 <107338809+sandhiya2203@users.noreply.github.com>
2026-07-15 13:03:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,65 @@
+console.log("NEW SCRIPT LOADED");
 async function loadPatientData()
 {
-
     try
     {
-
         const response = await fetch(
-    "https://cpppatient-monitorbackend-production.up.railway.app/patient?time=" 
-    + new Date().getTime()
-);
+            "/patient?time=" + new Date().getTime()
+        );
 
 
         const data = await response.json();
 
 
-        console.log("JSON DATA:", data);
+        console.log("API DATA:", data);
 
 
 
-        document.getElementById("status").innerHTML =
+        document.getElementById("status").textContent =
             data.status;
 
 
 
-        document.getElementById("heart").innerHTML =
+        document.getElementById("heart").textContent =
             data.heartRate;
 
 
 
-        document.getElementById("bp").innerHTML =
+        document.getElementById("bp").textContent =
             data.bloodPressure;
 
 
 
-        document.getElementById("spo2").innerHTML =
+        document.getElementById("spo2").textContent =
             data.spo2;
-
 
 
     }
 
     catch(error)
     {
-
-        console.log(
-            "JSON ERROR:",
-            error
-        );
-
+        console.log("ERROR:", error);
     }
-
-
 }
 
 
 
 function updateDateTime()
 {
-
     let now = new Date();
 
 
-    document.getElementById("date").innerHTML =
+    document.getElementById("date").textContent =
         now.toLocaleDateString();
 
 
-    document.getElementById("time").innerHTML =
+    document.getElementById("time").textContent =
         now.toLocaleTimeString();
-
-
 }
 
 
 
-
 setInterval(loadPatientData,1000);
-
 
 setInterval(updateDateTime,1000);
 
